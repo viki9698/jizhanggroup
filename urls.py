dd4619990ec2fefc171e447232a40d83eccff8c8
viki9698 <rui.zhang9698@gmail.com>
2014-05-08 17:03:19 +0800

    Signed-off-by: viki9698 <rui.zhang9698@gmail.com>

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import dbindexer
+from django.views.generic import TemplateView
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+# django admin
+admin.autodiscover()
+
+# search for dbindexes.py in all INSTALLED_APPS and load them
+dbindexer.autodiscover()
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', TemplateView.as_view(template_name="index.html")),
+    ('^admin/', include(admin.site.urls)),
+)
