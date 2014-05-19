e0b0f5c20a49ffeef6eeb5d22757d97170d32aba
viki9698 <rui.zhang9698@gmail.com>
2014-05-20 00:17:43 +0800

    update

--- groups/models.py
+++ groups/models.py
@@ -12,13 +12,25 @@ class Group(models.Model):
 
 class User(models.Model):
     username=models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
     displayName=models.CharField(max_length=50)
-    groups=models.ManyToManyField(Group)
+    email = models.CharField(max_length=50)
     user_type = models.CharField(max_length=10)
+    create_date=models.DateField('date published', blank=True, 
+        default=datetime.datetime.now().date())
+    def __unicode__(__self):
+        return __self.displayName
+
+class User_Group(models.Model):
+    user = models.ForeignKey(User)
+    group = models.ForeignKey(Group)
+    is_owner = models.CharField(max_length=1)
+
 class Bill(models.Model):
     title=models.CharField(max_length=20)
     description=models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date=models.DateField('date published', blank=True, 
+        default=datetime.datetime.now().date())
     group = models.ForeignKey(Group)
     
 class BillItem(models.Model):
